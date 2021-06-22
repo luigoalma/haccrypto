fcc62528a3525fb7270165c01702e03bfb453e34
Ian Burgwin <ian@ianburgwin.net>
2021-06-21 19:31:12 -0700

    disable manylinux2010 build (2014 includes this), disable ppc64le build

--- run-manylinux-build-containers.sh
+++ run-manylinux-build-containers.sh
@@ -14,15 +14,15 @@ function run_container {
 	docker run --rm -e PLAT=$PLAT -v `pwd`:/haccrypto quay.io/pypa/$PLAT $CMD /haccrypto/innerbuild.sh
 }
 
-run_container 2010 i686
-run_container 2010 x86_64
+#run_container 2010 i686
+#run_container 2010 x86_64
 
 run_container 2014 i686
 run_container 2014 x86_64
 run_container 2014 aarch64
-run_container 2014 ppc64le
+#run_container 2014 ppc64le
 
 run_container _2_24 i686
 run_container _2_24 x86_64
 run_container _2_24 aarch64
-run_container _2_24 ppc64le
+#run_container _2_24 ppc64le
