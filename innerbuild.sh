e8215321c4cf2e1965dd6f6bb2d54587571f5a41
ihaveahax <ian@ianburgwin.net>
2024-11-03 16:13:35 -0600

    add 3.12 and 3.13 to build-wheels-win.bat and innerbuild.sh for docker

--- innerbuild.sh
+++ innerbuild.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e -u -x
 
-pyvers=(cp36-cp36m cp37-cp37m cp38-cp38 cp39-cp39 cp310-cp310 cp311-cp311)
+pyvers=(cp36-cp36m cp37-cp37m cp38-cp38 cp39-cp39 cp310-cp310 cp311-cp311 cp312-cp312 cp313-313)
 
 function repair_wheel {
 	wheel="$1"
